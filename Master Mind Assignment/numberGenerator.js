05caf3b659ec14b0be05a451a345c1f021234905
Dil1797 <147470344+Dil1797@users.noreply.github.com>
2025-06-28 20:37:14 -0400

    Add files via upload

---
+++ Master Mind Assignment/numberGenerator.js
@@ -0,0 +1,9 @@
+//Name: Dil Humyra Sultana Borna
+//ID: 139166227
+//Date: 30/11/2024
+
+//function for generating random numbers
+
+function generateRandomNumber() {
+    return String(Math.floor(Math.random() * 10000)).padStart(4, '0');
+}
